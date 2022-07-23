98595b3f5573b281b31dc9343fc5d89d8af4877e
Dawid Majewski <dawidmajewski1991@gmail.com>
2022-07-23 14:05:08 +0200

    small devices nav

--- js/hamburger.js
+++ js/hamburger.js
@@ -5,8 +5,3 @@ hamburger.addEventListener('click', () => {
     const nav = document.querySelector('.navigation__list');
     nav.classList.toggle('navigation__list--visible');
 });
-
-navigation.addEventListener('click', () => {
-    const nav = document.querySelector('.navigation__list');
-    nav.classList.toggle('navigation__list--visible');
-});
