c27d57688d2b34952719ca43e627eb03343501aa
Dawid Majewski <dawidmajewski1991@gmail.com>
2022-07-23 11:22:58 +0200

    media queries small devices - navigation

---
+++ js/hamburger.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger--js');
+const navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation__list');
+    nav.classList.toggle('navigation__list--visible');
+});
+
+navigation.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation__list');
+    nav.classList.toggle('navigation__list--visible');
+});
